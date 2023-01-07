ad4a1417d3f71cc5e43067d3d41dd20b0dbe5f71
Otabek Rakhimov <rahimovmail@gmail.com>
2023-01-07 14:00:40 -0800

    Start the contents of the project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+import List from "./List";
+import Alert from "./Alert";
+
+function App() {
+  const [name, setName] = useState("");
+  const [list, setList] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  return <h2>grocery bud setup</h2>;
+}
+
+export default App;
